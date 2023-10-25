2ecc19ad3a2c3a840df2ee9b40f86b903525e8d9
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-10-25 11:13:34 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   useEffect(() => {
-    window.location.href = 'https://amzn.to/42aFW0T';
+    window.location.href = 'https://alterassumeaggravate.com/w7c1ptvucp?key=666fa264a72abcdd83daf0a290e22dd7';
   }, []);
 
   return null;
