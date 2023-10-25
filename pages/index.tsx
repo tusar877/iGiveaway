cc507eaa665bcab3b5560c4b01287c11ed792c59
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-10-25 15:05:12 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   useEffect(() => {
-    window.location.href = 'https://newsing.xyz';
+    window.location.href = 'https://alterassumeaggravate.com/w7c1ptvucp?key=666fa264a72abcdd83daf0a290e22dd7';
   }, []);
 
   return null;
